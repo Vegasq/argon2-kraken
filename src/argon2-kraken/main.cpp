7c66f31184fc25f1ed652f20bec28288f5f11fa0
Nick Yakovliev <mail@mkla.dev>
2023-04-30 13:46:29 -0500

    hash parser extracted into its own cpp

--- src/argon2-kraken/main.cpp
+++ src/argon2-kraken/main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <sstream>
 #include <map>
 #include <future>
 #include <algorithm>
@@ -14,8 +10,7 @@
 #include "argon2-cuda/processingunit.h"
 #include "argon2.h"
 
-#include "base64.cpp"
-#include "strings_tools.cpp"
+#include "hash_parser.cpp"
 
 
 template <class Device, class GlobalContext, class ProgramContext, class ProcessingUnit>
@@ -26,98 +21,6 @@ Device getDeviceToUse()
     return devices[0];
 }
 
-
-// Argon2ParamsData holds the parsed Argon2 hash parameters
-struct Argon2ParamsData
-{
-    argon2::Type type;
-    argon2::Version version;
-    std::string hash;
-    std::string salt;
-    std::uint32_t timeCost;
-    std::uint32_t memoryCost;
-    std::uint32_t parallelism;
-};
-
-// GetArgon2Type returns the Argon2 type based on the input token
-argon2::Type getArgon2Type(const std::string& token)
-{
-    if (token == "argon2i") return argon2::ARGON2_I;
-    if (token == "argon2d") return argon2::ARGON2_D;
-    if (token == "argon2id") return argon2::ARGON2_ID;
-    throw std::runtime_error("Unknown Argon2 type");
-}
-
-// GetArgon2Version returns the Argon2 version based on the input version number
-argon2::Version getArgon2Version(int version)
-{
-    if (version == 13) return argon2::ARGON2_VERSION_10;
-    if (version == 19) return argon2::ARGON2_VERSION_13;
-    throw std::runtime_error("Unsupported Argon2 version");
-}
-
-// ParseArgon2Hash parses the Argon2 hash string and returns an Argon2ParamsData structure
-Argon2ParamsData parseArgon2Hash(const std::string& argon2Hash)
-{
-    std::istringstream hashStream(argon2Hash);
-    std::string token;
-    std::vector<std::string> tokens;
-
-    // Split the input hash string into tokens
-    while (std::getline(hashStream, token, '$'))
-    {
-        if (!token.empty())
-        {
-            tokens.push_back(token);
-        }
-    }
-
-    if (tokens.size() != 5)
-    {
-        throw std::runtime_error("Failed to parse hash");
-    }
-
-    argon2::Type hashType = getArgon2Type(tokens[0]);
-    argon2::Version version = getArgon2Version(std::stoi(tokens[1].substr(2)));
-
-    std::istringstream complexityStream(tokens[2]);
-    std::string complexityToken;
-    uint32_t memCost = 0;
-    uint32_t timeCost = 0;
-    uint32_t parallelism = 0;
-
-    // Parse the memory cost, time cost, and parallelism
-    while (std::getline(complexityStream, complexityToken, ','))
-    {
-        std::istringstream tokenStream(complexityToken);
-        std::string key;
-        std::string value;
-
-        std::getline(tokenStream, key, '=');
-        std::getline(tokenStream, value, '=');
-
-        if (key == "m") memCost = std::stoi(value);
-        else if (key == "t") timeCost = std::stoi(value);
-        else if (key == "p") parallelism = std::stoi(value);
-    }
-
-    // Decode the salt and hash values
-    std::string decodedSalt = base64_decode(tokens[3]);
-    std::string decodedHash = base64_decode(tokens[4]);
-
-    // Build the Argon2ParamsData structure
-    Argon2ParamsData data = {
-        hashType,
-        version,
-        stringToHex(decodedHash),
-        decodedSalt,
-        timeCost,
-        memCost,
-        parallelism,
-    };
-
-    return data;
-}
 
 
 template <typename Device, typename GlobalContext, typename ProgramContext, typename ProcessingUnit>
