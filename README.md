47abd52be840ab13ad00cd943792850c3182b9f1
Nick Yakovliev <mail@mkla.dev>
2023-04-30 12:40:47 -0500

    WG replaced with futures

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
-# argon2-gpu [![build status](https://gitlab.com/omos/argon2-gpu/badges/master/build.svg)](https://gitlab.com/omos/argon2-gpu/commits/master)
+# argon2-kraken
 
-A proof-of-concept GPU password cracker for Argon2 hashes.
+Based on the project [argon2-gpu by Ondrej Mosnáček](https://gitlab.com/omos/argon2-gpu).
 
-[Argon2](https://github.com/P-H-C/phc-winner-argon2) is a password hashing function created by Alex Biryukov, Daniel Dinu, and Dmitry Khovratovich. It was designed to be resistant against brute-force attacks using specialized hardware, such as GPUs, ASICs, or FPGAs. In July 2015, it was announced as the winner of the [Password Hashing Competition](https://password-hashing.net).
+The [argon2-kraken](https://github.com/vegasq/argon2-kraken) program is a password cracking tool that uses the Argon2 hashing algorithm. The program can use either the OpenCL or CUDA library to run the Argon2 hashing algorithm on the GPU. The program takes in a leftlist file, a wordlist file, and a potfile, and outputs the successfully cracked passwords to the potfile.
 
-The main goal of this project is to provide an efficient GPU implementation of Argon2 that can be used to estimate the speed and efficiency of Argon2 GPU cracking, in order to support or refute claims of its GPU cracking resistance.
+This program implements an association attack technique inspired by [hashcat](https://github.com/hashcat/hashcat). Instead of comparing each line from the leftlist with each line from the wordlist, it associates each line from the leftlist with the line in the same position in the wordlist. This approach is intended to improve performance and reduce the amount of time required to crack passwords.
 
-## Backends
+## Usage
 
-Currently, the project implements two backends -- one that uses the NVIDIA's [CUDA](https://www.nvidia.com/object/cuda_home_new.html) framework and another one that uses the [OpenCL](https://www.khronos.org/opencl/) API.
+```
+argon2-kraken [mode: opencl or cuda] [leftlist] [wordlist] [potfile]
+```
 
-## Argon2 variants
+## TODO
 
-Argon2-gpu supports all Argon2 variants (Argon2i, Argon2d, and Argon2id) and versions (1.3 and 1.0).
-
-## Performance
-
-The CUDA implementation can reach about 40-60 GiB/s (divide by time cost * memory cost  * 1024 B to get hashes per second) on an NVIDIA Tesla K20X. For comparison, a fast Intel Xeon processor can only reach about 10 GiB/s.
+1. Bring the rest of CUDA kernel variants
+2. Add `precompute` argument
 
 ## Building
 
@@ -35,19 +34,3 @@ cmake -DCMAKE_BUILD_TYPE=Release .
 ```
 
 Finally, just run `make` to build the code. Note that to use the OpenCL backend, you need to have the `data` subdirectory in the working directory (if you have the binaries in a different directory, just create a symlink using `ln -s <path_to_repo>/data data`).
-
-### Building without CUDA support
-
-To build without CUDA support, just add `-DNO_CUDA=TRUE` to the CMake command line. In this case the CUDA backend will always report 0 devices.
-
-If CMake fails to find a usable CUDA installation, the project will be automatically built without CUDA support.
-
-## CUDA kernel variants
-
-The CUDA implementation has three variants, which are currently implemented in separate branches:
-
- * `master` -- uses only shared memory operations; is somewhat slower than the other two
- * `warp-shuffle` -- uses warp shuffle instructions; doesn't use shared memory at all
- * `warp-shuffle-shared` -- like `warp-shuffle`, but uses less regsters (compensated by using shared memory); this one is about as fast as `warp-shuffle`, but can be a little slower or faster in some edge cases
-
-In addition, Argon2i and Argon2id implementations support a special 'precompute' mode, which makes them as fast as Argon2d, but uses a bit more memory (depending on time cost and memory cost). This mode is also supported by the OpenCL backend and can be enabled/disabled at runtime.
--- src/argon2-kraken/main.cpp
+++ src/argon2-kraken/main.cpp
@@ -6,6 +6,8 @@
 #include <condition_variable>
 #include <sstream>
 #include <map>
+#include <future>
+#include <algorithm>
 
 #include "argon2-gpu-common/argon2params.h"
 #include "argon2-opencl/processingunit.h"
@@ -14,30 +16,18 @@
 
 #include "base64.cpp"
 #include "strings_tools.cpp"
-#include "waitgroup.cpp"
 
 
-template <class Device, class GlobalContext,
-          class ProgramContext, class ProcessingUnit>
-argon2::opencl::Device getOpenCLDeviceToUse()
+template <class Device, class GlobalContext, class ProgramContext, class ProcessingUnit>
+Device getDeviceToUse()
 {
     GlobalContext global;
     auto &devices = global.getAllDevices();
-
-    return devices[0];
-}
-
-template <class Device, class GlobalContext,
-          class ProgramContext, class ProcessingUnit>
-argon2::cuda::Device getCUDADeviceToUse()
-{
-    GlobalContext global;
-    auto &devices = global.getAllDevices();
-
     return devices[0];
 }
 
 
+// Argon2ParamsData holds the parsed Argon2 hash parameters
 struct Argon2ParamsData
 {
     argon2::Type type;
@@ -49,106 +39,80 @@ struct Argon2ParamsData
     std::uint32_t parallelism;
 };
 
-Argon2ParamsData parse_argon2_hash(const std::string &argon2_hash)
+// GetArgon2Type returns the Argon2 type based on the input token
+argon2::Type getArgon2Type(const std::string& token)
 {
-    // std::cout << argon2_hash;
-    std::istringstream iss(argon2_hash);
-    std::string token;
+    if (token == "argon2i") return argon2::ARGON2_I;
+    if (token == "argon2d") return argon2::ARGON2_D;
+    if (token == "argon2id") return argon2::ARGON2_ID;
+    throw std::runtime_error("Unknown Argon2 type");
+}
 
+// GetArgon2Version returns the Argon2 version based on the input version number
+argon2::Version getArgon2Version(int version)
+{
+    if (version == 13) return argon2::ARGON2_VERSION_10;
+    if (version == 19) return argon2::ARGON2_VERSION_13;
+    throw std::runtime_error("Unsupported Argon2 version");
+}
+
+// ParseArgon2Hash parses the Argon2 hash string and returns an Argon2ParamsData structure
+Argon2ParamsData parseArgon2Hash(const std::string& argon2Hash)
+{
+    std::istringstream hashStream(argon2Hash);
+    std::string token;
     std::vector<std::string> tokens;
 
-    while (std::getline(iss, token, '$'))
+    // Split the input hash string into tokens
+    while (std::getline(hashStream, token, '$'))
     {
         if (!token.empty())
         {
-            // std::cout << token << "\n";
             tokens.push_back(token);
         }
     }
 
     if (tokens.size() != 5)
     {
-        printf("\nBROKEN HASH\n");
+        throw std::runtime_error("Failed to parse hash");
     }
 
-    argon2::Type hash_type = argon2::ARGON2_I;
-    if (tokens[0] == "argon2i")
-    {
-        hash_type = argon2::ARGON2_I;
-    }
-    else if (tokens[0] == "argon2d")
-    {
-        hash_type = argon2::ARGON2_D;
-    }
-    else if (tokens[0] == "argon2id")
-    {
-        hash_type = argon2::ARGON2_ID;
-    }
-    else
-    {
-        printf("\nUnknown hash type\n");
-    }
+    argon2::Type hashType = getArgon2Type(tokens[0]);
+    argon2::Version version = getArgon2Version(std::stoi(tokens[1].substr(2)));
 
-    // VERSION
-    int version = std::stoi(tokens[1].substr(2));
-    argon2::Version aVer;
-    if (version == 13)
-    {
-        aVer = argon2::ARGON2_VERSION_10;
-    }
-    else if (version == 19)
-    {
-        aVer = argon2::ARGON2_VERSION_13;
-    }
-
-    // printf("VERSION %d\n", version);
-
-    // PARAMS
-    std::istringstream iss2(tokens[2]);
-    std::string token2;
-
-    uint32_t mem_cost = 0;
-    uint32_t time_cost = 0;
+    std::istringstream complexityStream(tokens[2]);
+    std::string complexityToken;
+    uint32_t memCost = 0;
+    uint32_t timeCost = 0;
     uint32_t parallelism = 0;
 
-    while (std::getline(iss2, token2, ','))
+    // Parse the memory cost, time cost, and parallelism
+    while (std::getline(complexityStream, complexityToken, ','))
     {
-        std::istringstream token_iss(token2);
+        std::istringstream tokenStream(complexityToken);
         std::string key;
         std::string value;
 
-        std::getline(token_iss, key, '=');
-        std::getline(token_iss, value, '=');
+        std::getline(tokenStream, key, '=');
+        std::getline(tokenStream, value, '=');
 
-        if (key == "m")
-        {
-            mem_cost = std::stoi(value);
-        }
-        else if (key == "t")
-        {
-            time_cost = std::stoi(value);
-        }
-        else if (key == "p")
-        {
-            parallelism = std::stoi(value);
-        }
+        if (key == "m") memCost = std::stoi(value);
+        else if (key == "t") timeCost = std::stoi(value);
+        else if (key == "p") parallelism = std::stoi(value);
     }
 
-    std::string decoded_salt = base64_decode(tokens[3]);
-    std::string decoded_hash = base64_decode(tokens[4]);
-    const void *salt_c = static_cast<const void *>(decoded_salt.c_str());
-    std::size_t salt_size_c = decoded_salt.length();
+    // Decode the salt and hash values
+    std::string decodedSalt = base64_decode(tokens[3]);
+    std::string decodedHash = base64_decode(tokens[4]);
 
-    std::size_t decoded_salt_size_c = decoded_salt.length();
-    const void *decoded_salt_c = reinterpret_cast<const void *>(decoded_salt.data());
-
+    // Build the Argon2ParamsData structure
     Argon2ParamsData data = {
-        hash_type,
-        aVer,
-        stringToHex(decoded_hash),
-        decoded_salt,
-        time_cost,
-        mem_cost,
+        hashType,
+        version,
+        stringToHex(decodedHash),
+        decodedSalt,
+        timeCost,
+        memCost,
         parallelism,
     };
 
@@ -156,19 +120,18 @@ Argon2ParamsData parse_argon2_hash(const std::string &argon2_hash)
 }
 
 
-int VerifyHashOpenCL(
+template <typename Device, typename GlobalContext, typename ProgramContext, typename ProcessingUnit>
+int compareHashImpl(
     const std::vector<std::string> &passwords, 
     const std::string &hash, 
     const argon2::Argon2Params &params, 
-    std::string mode, 
     argon2::Type &type, 
     argon2::Version &version
 ){
-    argon2::opencl::Device device = getOpenCLDeviceToUse<argon2::opencl::Device, argon2::opencl::GlobalContext,
-                                                        argon2::opencl::ProgramContext, argon2::opencl::ProcessingUnit>();
-    argon2::opencl::GlobalContext global;
-    argon2::opencl::ProgramContext progCtx(&global, {device}, type, version);
-    argon2::opencl::ProcessingUnit processingUnit(&progCtx, &params, &device, passwords.size(), false, false);
+    Device device = getDeviceToUse<Device, GlobalContext, ProgramContext, ProcessingUnit>();
+    GlobalContext global;
+    ProgramContext progCtx(&global, {device}, type, version);
+    ProcessingUnit processingUnit(&progCtx, &params, &device, passwords.size(), false, false);
     std::unique_ptr<uint8_t[]> computedHash(new uint8_t[params.getOutputLength() * passwords.size()]);
 
     for (std::size_t i = 0; i < passwords.size(); i++) {
@@ -185,131 +148,126 @@ int VerifyHashOpenCL(
             return i;
         }
     }
-    return -1;
-}
-
-
-int VerifyHashCUDA(
-    const std::vector<std::string> &passwords, 
-    const std::string &hash, 
-    const argon2::Argon2Params &params, 
-    std::string mode, 
-    argon2::Type &type, 
-    argon2::Version &version
-){
-    argon2::cuda::Device device = getCUDADeviceToUse<argon2::cuda::Device, argon2::cuda::GlobalContext,
-                                                        argon2::cuda::ProgramContext, argon2::cuda::ProcessingUnit>();
-    argon2::cuda::GlobalContext global;
-    argon2::cuda::ProgramContext progCtx(&global, {device}, type, version);
-    argon2::cuda::ProcessingUnit processingUnit(&progCtx, &params, &device, passwords.size(), false, false);
-    std::unique_ptr<uint8_t[]> computedHash(new uint8_t[params.getOutputLength() * passwords.size()]);
-
-    for (std::size_t i = 0; i < passwords.size(); i++) {
-        processingUnit.setPassword(i, passwords[i].data(), passwords[i].size());
-    }
-    
-    processingUnit.beginProcessing();
-    processingUnit.endProcessing();
-
-    for (std::size_t i = 0; i < passwords.size(); i++) {
-        processingUnit.getHash(i, computedHash.get() + i * params.getOutputLength());
-
-        if (std::memcmp(hash.data(), computedHash.get() + i * params.getOutputLength(), params.getOutputLength()) == 0) {
-            return i;
-        }
-    }                                    
 
     return -1;
 }
 
-
-int VerifyHash(
-    const std::vector<std::string> &passwords, 
-    const std::string &hash, 
-    const argon2::Argon2Params &params, 
-    std::string mode, 
-    argon2::Type &type, 
-    argon2::Version &version
-){
-    if (mode == "opencl") {
-        return VerifyHashOpenCL(passwords, hash, params, mode, type, version);
-    } else if (mode == "cuda") {
-        return VerifyHashCUDA(passwords, hash, params, mode, type, version);
-    }
-
-
-    return -1;
-}
-
-extern "C" int Compare(std::string mode, std::string hash, const std::vector<std::string> &passwords)
+int Compare(const std::string &mode, const std::string &hash, const std::vector<std::string> &passwords)
 {
-    Argon2ParamsData paramsData = parse_argon2_hash(hash);
+    Argon2ParamsData paramsData = parseArgon2Hash(hash);
 
-    uint salt_size = paramsData.salt.length();
+    uint saltSize = paramsData.salt.length();
 
-    if (paramsData.salt[salt_size-1] == 0) {
-        salt_size -= 1;
+    // TODO: Why do we get null in it and why .length() counts it in?
+    if (paramsData.salt[saltSize-1] == 0) {
+        saltSize -= 1;
     }
 
-    const void *salt2_c = static_cast<const void *>(paramsData.salt.c_str());
+    const void *salt_pointer = static_cast<const void *>(paramsData.salt.c_str());
     argon2::Argon2Params params(
         paramsData.hash.length() / 2, 
-        salt2_c, salt_size, 
+        salt_pointer, saltSize, 
         nullptr, 0, 
         nullptr, 0, 
         paramsData.timeCost, paramsData.memoryCost, paramsData.parallelism);
 
-    std::string hex_hash = hexToString(paramsData.hash);
-    return VerifyHash(passwords, hex_hash, params, mode, paramsData.type, paramsData.version);
-}
+    std::string hexHash = hexToString(paramsData.hash);
 
+    if (mode == "opencl") {
+        return compareHashImpl<argon2::opencl::Device, argon2::opencl::GlobalContext, argon2::opencl::ProgramContext, argon2::opencl::ProcessingUnit>(
+            passwords, hexHash, params, paramsData.type, paramsData.version
+        );
+    } else if (mode == "cuda") {
+        return compareHashImpl<argon2::cuda::Device, argon2::cuda::GlobalContext, argon2::cuda::ProgramContext, argon2::cuda::ProcessingUnit>(
+            passwords, hexHash, params, paramsData.type, paramsData.version
+        );
+    }
+
+    return -1;
+}
 
 std::map<std::string, std::vector<std::string>> buildTasks(std::string leftlist, std::string wordlist){
     // TODO: memory concerns
 
     // Open the input files.
-    std::ifstream ll_file(leftlist);
-    if (!ll_file.is_open()) {
-        throw std::runtime_error("Cannot open ll_file.txt");
+    std::ifstream llFile(leftlist);
+    if (!llFile.is_open()) {
+        throw std::runtime_error("Cannot open llFile");
     }
 
-    std::ifstream wl_file(wordlist);
-    if (!wl_file.is_open()) {
-        throw std::runtime_error("Cannot open wl_file.txt");
+    std::ifstream wlFile(wordlist);
+    if (!wlFile.is_open()) {
+        throw std::runtime_error("Cannot open wlFile");
     }
 
     std::map<std::string, std::vector<std::string>> data;
 
     // Read the input files and add jobs to the vector.
     std::string hash, plain;
-    while (std::getline(ll_file, hash) && std::getline(wl_file, plain)) {
+    while (std::getline(llFile, hash) && std::getline(wlFile, plain)) {
         plain = plain.substr(0, plain.length() - 1);
         data[hash].push_back(plain);
     }
 
-    ll_file.close();
-    wl_file.close();
+    llFile.close();
+    wlFile.close();
 
     return data;
 }
 
 // Worker function that takes a task and a mutex to protect the output stream
 void worker(
-    const std::string& task_name, 
-    const std::vector<std::string>& task_data, 
+    const std::string& taskName, 
+    const std::vector<std::string>& taskData, 
     std::string mode,
     std::ofstream& outfile, 
-    std::mutex& out_mutex, 
-    WaitGroup& wg
+    std::mutex& outMutex
 ) {
-    int i = Compare(mode, task_name, task_data);
+    int i = Compare(mode, taskName, taskData);
     if (i >= 0) {
         // Lock the output stream before writing to it
-        std::unique_lock<std::mutex> lock(out_mutex);
-        outfile << task_name << ":" << task_data[i] << "\n";
+        std::unique_lock<std::mutex> lock(outMutex);
+        outfile << taskName << ":" << taskData[i] << "\n";
+    }
+}
+
+void processTasks(
+    const std::map<std::string, std::vector<std::string>> &tasks,
+    const std::string &mode,
+    const std::string &outputFile
+) {
+    std::ofstream outfile(outputFile);
+    std::mutex outMutex;
+
+    const int max_workers = 60;
+    std::vector<std::future<void>> futures;
+
+    for (const auto &task : tasks) {
+        // Wait for a worker to finish if the maximum number of active workers is reached
+        while (futures.size() >= max_workers) {
+            auto it = std::remove_if(futures.begin(), futures.end(), [](std::future<void> &f) {
+                return f.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+            });
+
+            futures.erase(it, futures.end());
+
+            if (futures.size() < max_workers) {
+                break;
+            }
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+
+        // Create a new worker using std::async
+        futures.emplace_back(std::async(std::launch::async, worker, task.first, task.second, mode, std::ref(outfile), std::ref(outMutex)));
     }
 
-    wg.done();
+    // Wait for all remaining futures to complete
+    for (auto &f : futures) {
+        f.get();
+    }
+
+    outfile.close();
 }
 
 int main(int argc, const char *const *argv) {
@@ -320,34 +278,8 @@ int main(int argc, const char *const *argv) {
 
     // Build the tasks map
     std::map<std::string, std::vector<std::string>> tasks = buildTasks(argv[2], argv[3]);
-    // Open the output file stream
-    std::ofstream outfile(argv[4]);
 
-    // Create a mutex to protect the output stream
-    std::mutex out_mutex;
-
-    // Create a condition variable to wait for workers to finish
-    // std::condition_variable cv;
-
-    // Limit the number of active workers to 4
-    const int max_workers = 60;
-    WaitGroup wg;
-
-    // Loop over the tasks and create a worker thread for each one
-    for (const auto &task : tasks) {
-        // Wait for a worker to finish if we have reached the maximum number of active workers
-        while (wg.size() > max_workers) {}
-
-        // Create a new worker thread
-        std::thread t(worker, task.first, task.second, argv[1], std::ref(outfile), std::ref(out_mutex), std::ref(wg));
-        wg.add(1);
-        t.detach();
-    }
-
-    // std::cout << "doune with dispatching\n";
-    wg.wait();
-
-    outfile.close();
+    processTasks(tasks, argv[1], argv[4]);
 
     std::cout << "Done\n";
     return 0;
